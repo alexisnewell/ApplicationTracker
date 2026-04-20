a9f7b8dd18a0877d7b89eb98c6ac8bfee2f34bed
Alexis Newell <alexis.newell@ucalgary.ca>
2026-04-19 20:53:27 -0600

    has pages

---
+++ app_tracker/app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+export default function Dashboard(){
+    return <div>My Dashboard</div>;
+
+}
--- app_tracker/app/page.tsx
+++ app_tracker/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,11 +6,11 @@ export default function Home() {
     <main className="flex-1">
       <section className="container mx-auto px-4 py-32">
         <div className="mx-auto max-w-4xl text-center">
-        <h1 className="text-black mb-6 text 6-xl font-bold ">Track Your Job Applications</h1>
+        <h1 className="text-black mb-6 text-6xl font-bold ">Track Your Job Applications!</h1>
         <p className="text-muted-foreground mb-10 text-xl">Organize all of your Applications in one place!</p>
         <div className="flex flex-col items-center gap-4">
-          <button>Get Started!</button>
-          <p>Completely Free and Easy!</p>
+          <Button className="px-8 py-4 text-lg h-auto bg-[#008542] hover:bg-[#006d36] text-white">Get Started! →</Button>
+          <p className="text-muted-foreground mb-10 text-xl">Completely Free and Easy!</p>
         </div>
         </div>
       </section>
